b4575da3678697aae6bb259329c05d1861776ec4
Arpit Gupta <rahul21527@gmail.com>
2018-03-12 16:28:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-module.exports = function(context,y) {
+module.exports = function(context) {
     context.log('Node.js Blob trigger function processed');
     context.bindings.myOutputBlob=context.bindings.myInputBlob+context.bindingData.blobTrigger+"  "+context.bindings.myBlob.length+'\n';
     context.done();
